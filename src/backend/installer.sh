6754ead3c3555c43a9f109dd8ea135623627094b
Hyunwook Shin <shincontact@icloud.com>
2017-12-04 09:06:35 +0000

    Change .tgz to .zip

--- src/backend/installer.sh
+++ src/backend/installer.sh
@@ -11,8 +11,8 @@ if [ "$1" == "" ]; then
    exit 1
 fi
 
-if [ ! -f cmpe281p2.tgz ]; then
-   echo "Please copy cmpe281p2.tgz file to the current directory"
+if [ ! -f cmpe281p2.zip ]; then
+   echo "Please copy cmpe281p2.zip file to the current directory"
    exit 1
 fi
 
@@ -29,7 +29,8 @@ sudo pip install MySQL-python
 
 echo "Deploying Application..."
 sudo rm -rf /tmp/deploy
-sudo tar -xvf cmpe281p2.tgz -C /tmp/deploy
+sudo mkdir /tmp/deploy
+sudo unzip cmpe281p2.zip -d /tmp/deploy
 
 sudo rm -rf /cmpe281p1
 sudo mkdir -p /cmpe281p2
--- src/frontend/installer.sh
+++ src/frontend/installer.sh
@@ -5,8 +5,8 @@
 # for Cloud Project #2
 # ########################
 
-if [ ! -f cmpe281p2.tgz ]; then
-   echo "Please copy cmpe281p2.tgz file to the current directory"
+if [ ! -f cmpe281p2.zip ]; then
+   echo "Please copy cmpe281p2.zip file to the current directory"
    exit 1
 fi
 
@@ -21,7 +21,8 @@ sudo chkconfig httpd on
 
 echo "Deploying Application..."
 sudo rm -rf /tmp/deploy
-sudo tar -xvf cmpe281p2.tgz -C /tmp/deploy
+sudo mkdir /tmp/deploy
+sudo unzip cmpe281p2.zip -d /tmp/deploy
 
 sudo cp -rf /tmp/deploy/frontend/* /var/www/html
 echo "Frontend installer complete!"
